97472717c49f44e04498019f6981d21a1f164c4f
Kiryl <admin@some.com>
2024-06-01 20:54:59 +0200

    edited settings.py

--- cut_it/settings.py
+++ cut_it/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fr^i%66bb4)zs=m9@oyh#*zg$04bc)uobm_4t+03-x^a7ul=r*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
